ae5ff4bc881894c8d7cc60ba939c292081bf19f3
sudip34 <sudipcep2006@gmail.com>
2024-09-22 17:23:21 +0200

    updated File handeling to store json data

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Inside main in todo app")
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	// load the file
+	storage.Load(&todos)
 	todos.add("Buy milk")
 	todos.add("Buy bread")
 	todos.add("Buy benana")
 	todos.toggle(0)
 	todos.print()
+	storage.save(todos)
 }
